1186424972db9b9a73a3552fd113573ae2cc4d79
zen_leo <leo.in.empty@gmail.com>
2018-10-17 10:44:42 +0300

    Тест для случая HTTPS

---
+++ test/testYmapsNodeWrapHTTPS.ps1
@@ -0,0 +1,10 @@
+﻿Clear-Host
+
+$url = 'https://77.222.54.235:2370/route?apikey=IQTCgkwwGXEIGNtwka6J3li5xg2G8Ds1&waypoints=Москва|Ростов-на-Дону|Кемерово|Краснодар'
+$Request = [System.Net.HttpWebRequest]::CreateHttp($url)
+$Request.Method = 'GET'
+$Request.ServerCertificateValidationCallback = {$true}
+$response = $Request.GetResponse().GetResponseStream()
+$sr = New-Object System.IO.StreamReader($response) 
+$respTxt = $sr.ReadToEnd()
+$respTxt
