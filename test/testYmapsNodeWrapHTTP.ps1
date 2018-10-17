f1ce3592e76e58553b21e6ea5c1eda0ec10242ff
zen_leo <leo.in.empty@gmail.com>
2018-10-17 11:47:29 +0300

    Разделение тестов для HTTP и HTTPS сервисов.

--- test/testYmapsNodeWrapHTTP.ps1
+++ test/testYmapsNodeWrapHTTP.ps1
@@ -1,9 +1,5 @@
 ﻿Clear-Host
 
-#$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
-#[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
-[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
-
 $pointsFromArray = '55.811511,37.312518','Ростов-на-Дону','Батайск','Абаза','Арзамас','Белозерск','Беслан','Буйнакск'
 $pointsToArray = 'Краснодар','Азов','Кемерово','Абакан','Аксай','Барнаул','Волжск'
 
--- test/testYmapsNodeWrapHTTPS.ps1
+++ test/testYmapsNodeWrapHTTPS.ps1
@@ -1,10 +1,33 @@
 ﻿Clear-Host
 
-$url = 'https://77.222.54.235:2370/route?apikey=IQTCgkwwGXEIGNtwka6J3li5xg2G8Ds1&waypoints=Москва|Ростов-на-Дону|Кемерово|Краснодар'
-$Request = [System.Net.HttpWebRequest]::CreateHttp($url)
-$Request.Method = 'GET'
-$Request.ServerCertificateValidationCallback = {$true}
-$response = $Request.GetResponse().GetResponseStream()
-$sr = New-Object System.IO.StreamReader($response) 
-$respTxt = $sr.ReadToEnd()
-$respTxt
+$pointsFromArray = '55.811511,37.312518','Ростов-на-Дону','Батайск','Абаза','Арзамас','Белозерск','Беслан','Буйнакск'
+$pointsToArray = 'Краснодар','Азов','Кемерово','Абакан','Аксай','Барнаул','Волжск'
+
+Write-Host 'Start load test'
+
+foreach ($pointFrom in $pointsFromArray){
+
+    foreach ($pointTo in $pointsToArray){
+
+        $url = 'https://77.222.54.235:2370/route?apikey=IQTCgkwwGXEIGNtwka6J3li5xg2G8Ds1&waypoints='+ $pointFrom + '|' + $pointTo
+
+        $request = [System.Net.HttpWebRequest]::CreateHttp($url)
+        $request.Method = 'GET'
+        $request.ServerCertificateValidationCallback = {$true}
+
+        $responseObj = $request.GetResponse()
+        $responseStreamObj = $responseObj.GetResponseStream()
+
+        $responseStream = New-Object System.IO.StreamReader($responseStreamObj) 
+  
+        $responseStr = $responseStream.ReadToEnd()
+
+        Write-Host $responseStr
+ 
+        $responseObj.Close()
+        $responseStreamObj.Close()
+   
+   }
+}
+
+Write-Host 'End load test'
